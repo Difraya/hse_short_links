45042fd9f0d69f3227d0b7a0e4414cc1f067124e
Julia <diffary96@gmail.com>
2025-03-31 02:00:51 +0300

    добавлены изменения и настроен Celery с Redis

--- celery_app.py
+++ celery_app.py
@@ -1,12 +1,23 @@
 from celery import Celery
 from celery.schedules import crontab
 from config import REDIS_BROKER_URL
+import multiprocessing
+import platform
+from sqlalchemy.future import select
+from database import get_async_session
+from models import Links
+from datetime import datetime, timezone
+
+if platform.system() == "Windows":
+    multiprocessing.set_start_method("spawn", force=True)
+
+# celery = Celery("tasks", broker="redis://localhost:6379/0")
 
 celery = Celery(
-    "tasks",
+    "celery_app",
     broker=REDIS_BROKER_URL,
     backend=REDIS_BROKER_URL,
-    include=["tasks"]
+    include=["celery_app"]
 )
 
 celery.conf.beat_schedule = {
@@ -15,3 +26,18 @@ celery.conf.beat_schedule = {
         "schedule": crontab(minute=0, hour="*"),  # каждый час
     },
 }
+
+@celery.task(name="tasks.delete_expired_links")
+def delete_expired_links():
+    import asyncio
+
+    async def _delete():
+        async with get_async_session() as session:
+            result = await session.execute(select(Links))
+            links = result.scalars().all()
+            for link in links:
+                if link.expires_at and datetime.now(timezone.utc) > link.expires_at:
+                    await session.delete(link)
+            await session.commit()
+
+    asyncio.run(_delete())
